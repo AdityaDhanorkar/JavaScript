b56d39306e73da4137a4dcd0aab45e38572776da
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-01-30 10:16:54 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use Strict";//treat all Js code as newer Version
+
+// alert(3 + 3) //We are Using a NodeJs Not Browser
+
+// Datatypes In JaVaScript Are Follows
+/*
+    Primitive Data Types :-
+    1] number => 2 to power 53
+    2] bigint
+    3] string ""
+    4] null=>standalone Value
+    5] undefined
+    6] symbol => Unique
+    7] Boolean=> True OR False
+
+    Non-Primitive data types
+    Objects
+*/
+let age =21
+let name = "Raghu"
+let city //Typeof undefined is undefined
+let state = null //TypeOf null is "Object" Imp for Interview
+
+console.log(age)
+console.log(name)
+console.log(city)
+console.log(state)
+
+console.table([typeof age ,typeof name ,typeof city ,typeof state])
+
+console.log(typeof undefined)//Undefined
+console.log(typeof null);//Object IMPORTANT
