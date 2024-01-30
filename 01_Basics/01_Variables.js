3e829df72599c01a5689e0d3fc5f1d2684c9f47c
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-01-30 09:30:28 +0000

    container config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 261261
+var accountMail ="raghu@google.com"
+let accountPass = "907597"
+city = "Washim"
+let accountState;
+// accountId = 93097136 //We Can't update The Constant Variable once We Declared
+
+console.log(accountId);
+
+console.table([accountId ,accountMail,accountPass,city])
+console.log("After Updations")
+accountMail="abcd@mail.com"
+accountPass="7972"
+city = "Pune"
+
+console.table([accountId , accountMail,accountPass,city,accountState])
