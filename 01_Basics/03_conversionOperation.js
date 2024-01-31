fd8eb9961450534c6c137d7b1d985b136a861166
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-01-31 10:04:04 +0000

    Convertion Of Datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,11 +25,11 @@ let NumberInBoolean = ""
 
 let OriginalBoolean = Boolean(NumberInBoolean)
 
-console.log(typeof NumberInBoolean);
+// console.log(typeof NumberInBoolean);
 
-console.log(typeof OriginalBoolean);
+// console.log(typeof OriginalBoolean);
 
-console.log(OriginalBoolean);
+// console.log(OriginalBoolean);
 
  /*
    When We Convert In Boolean then What Happen given Below
@@ -39,3 +39,42 @@ console.log(OriginalBoolean);
    1=>true
    0=>false
  */
+
+  //  *****************Operations*********************
+
+  let value=2
+  let negValue=-value
+  // console.log(negValue);
+
+  // console.log(2+3);
+  // console.log(5-3);
+  // console.log(5*5);
+  // console.log(10/2);
+  // console.log(2**3);
+  // console.log(18%2);
+
+  let str1 = "Aditya"
+  let str2 = " Dhanorkar"
+  let str3 = " Parshram"
+  // console.log(str1 +str3+ str2);
+
+  // console.log(2 +"3");
+  // console.log("3" + 6 );
+  // console.log("3"+"3");
+  // console.log("5" + 5 +4);
+  // console.log(5 + 4 + "5");
+
+  // console.log(true);
+  // console.log(+true);
+  // console.log(false);
+  // console.log(+false);
+  // console.log(+"");
+  // console.log("");
+
+  let num1 , num2 , num3
+  num1 = num2 = num3 = 2 + 2
+  // console.table([num1,num2,num3])
+
+  let gameCounter = 100
+  ++gameCounter;
+  // console.log(gameCounter);
