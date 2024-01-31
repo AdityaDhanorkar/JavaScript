fc22db25e42f53e29d08afa8c93937a16f9fd348
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-01-31 10:57:46 +0000

    Comparison

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,23 @@
+// console.log(2==2);
+// console.log(2>5);
+// console.log(5<8)
+// console.log(8>=9);
+// console.log(10<=15);
+
+// console.log("3" > 2);
+// console.log("25" < 8);
+
+// console.log(null >0); 
+// console.log(null == 0);
+// console.log(null <=0);
+/* "> , < , >= , <=" is "Comparisons" in which they connvert "null" into the "0" but "==" is "equivality"  operator in which check equivality they do't convert "null" into "0" */
+
+// console.log(undefined==0);
+// console.log(undefined<0);
+// console.log(undefined>=0);
+
+
+//===
+console.log("2" === "2");//strict Check in which they "Check the value as well as Datatypes"
+
+//== in which "only check the Value" irrespective of their Datatypes
