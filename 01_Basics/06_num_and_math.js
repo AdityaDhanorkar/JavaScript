0f596ae17749d7b4788704cc558a315f9a103085
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-02-05 08:20:08 +0000

    Number and Math

---
+++ 01_Basics/06_num_and_math.js
@@ -0,0 +1,31 @@
+let number = 123.765;
+// console.log(number);
+const other = new Number(100)
+// console.log(other);
+
+// console.log(number.toString().length);
+
+// console.log(number.toFixed(2));
+
+// console.log(number.toPrecision(4));
+const paisa = 1000000
+// console.log(paisa.toLocaleString('en-IN'));
+
+// ++++++++++++++ Maths ++++++++++++++++++++++++
+
+console.log(Math);
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.max( 2 , 8 ,10));
+// console.log(Math.min(1,8,9,3,0.5));
+// console.log(Math.random());
+
+// console.log((Math.random()*10 ) + 1);
+
+console.log(Math.floor(Math.random()*10)+1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * max-min + 1 ) +  min);
