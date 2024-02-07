48179ba6a7a07f268b336c669dd2a0aa68902ee7
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-02-07 17:17:02 +0000

    If Statement In JS

---
+++ 04_ControlFlow/01_one.js
@@ -0,0 +1,62 @@
+//IF
+
+// const userExecute = true
+// const userExecute = false
+// if(userExecute){
+//     console.log("SuccesFully Loged In");
+// }
+
+// const temperature = 50
+
+// if(temperature <50){
+//     console.log("Temp Is Less 50");
+// }
+// else{
+//     console.log("Temp Is Greater Than 50");
+// }
+
+// console.log("Always Execute");
+
+// const UseID ="Raghu"
+// if(UseID =="Raghu"){
+//     const name = "Aditya"
+//     console.log(`${UseID} logIn SuccesFully`);
+// }
+    // console.log(name);
+
+// < , > , <= , >= ,== ,=== ,!= ,!==
+
+// const logIn = true
+// const userId = true
+// const passWord ="123"
+
+
+// if(logIn && userId && passWord =="1234"){
+//     console.log("Log IN SuccesFully");
+// }else{
+//     console.log("Wrong ParaMeter");
+// }
+
+// const LogInThrEmail = false
+// const LogInThrGoogle = false
+
+// if(LogInThrEmail || LogInThrGoogle){
+//     console.log("Log In SuccesFully");
+// }else{
+//     console.log("Not  Logging");
+// }
+
+// const score = 1000
+
+// if(score <100){
+//     console.log("Score Is less than 100");
+// }else if (score <500) {
+//     console.log("Score IS Less THan 500");
+// }else if (score <900) {
+//     console.log("Score Is Less Than 900");
+// }else if (score <1200) {
+//     console.log("Score IS greater Than 1000");
+// }
+// else{
+//     console.log("Score Is Equal to Score");
+// }
