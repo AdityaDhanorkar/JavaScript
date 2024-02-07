edb4deb7514b9ddaed85bd96554cbe586504eab4
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-02-07 05:20:42 +0000

    Scope In Js

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,18 @@
+// var a = 10
+let b = 20
+// const c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+if(true)
+{
+    let b = 200
+    // const c = 60
+    // var a = 40
+    console.log("Inner :" ,b);
+}
+
+// console.log(a);
+console.log(b);
+// console.log(c);
