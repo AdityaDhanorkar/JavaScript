d8e225adf46ef394417501e6e4bd0a158a034801
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-02-07 05:57:03 +0000

    Scope In Js

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -10,9 +10,48 @@ if(true)
     let b = 200
     // const c = 60
     // var a = 40
-    console.log("Inner :" ,b);
+    // console.log("Inner :" ,b);
 }
 
 // console.log(a);
-console.log(b);
+// console.log(b);
 // console.log(c);
+
+
+function UserName()  {
+     const usr = "Aditya"
+
+     function Channel(){
+        const ChannelName = "Coffe With Adi"
+        // console.log(usr);
+     }
+     Channel()
+    //  console.log(ChannelName);
+
+}
+UserName()
+
+if(true){
+    const Name = "Raghu"
+
+    if(Name === "Raghu"){
+        const Last ="Dhanorkar"
+        // console.log(Name);
+        // console.log(Last);
+    }
+    // console.log(Last);
+    // console.log(Name);
+}
+
+// console.log(addOne(5))  //Acces Before Initialization
+
+function addOne(num){
+    return num+1
+}
+
+
+// console.log(addTwo(5)) //Can't Acess Before initialization
+
+const addTwo = function(num){
+    return num+2
+}
