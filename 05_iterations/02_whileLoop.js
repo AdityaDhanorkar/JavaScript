0ab2c3e946e89619eec54cb6233803bb20f8cbcf
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-02-08 07:10:18 +0000

    While Loop In Js

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,32 @@
+//WHILE LOOP
+
+// while (condition) {
+    
+// }
+
+
+// let index =0
+// while(index <= 10){
+//     console.log(`Index Value Is ${index}`);
+//     index = index+2
+// }
+
+// const arr =["Aditya" , "Raghu" , "Dhanorkar" , "Patil"]
+// let index = 0
+// while(index < arr.length){
+//     console.log(arr[index]);
+//     index ++
+// }
+
+
+//****************** Do While *********************
+
+// do {
+    
+// } while (condition);
+
+// let score = 51
+// do{
+//     console.log(`My Score Is ${score}`)
+//     score = score+1
+// }while(score <50)
