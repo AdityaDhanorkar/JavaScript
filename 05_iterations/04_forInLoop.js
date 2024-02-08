33e271e6c719b55585872365342e011ca4d35592
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-02-08 08:58:48 +0000

    For In Loop In Js

---
+++ 05_iterations/04_forInLoop.js
@@ -0,0 +1,42 @@
+//FOR IN LOOP
+
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+
+//********** For In Loop On Object ****************
+
+const myOnbject = {
+    js : "Javascript",
+    cpp :"C++",
+    py :"Python",
+    rb : "Ruby",
+    swift :"Swift For Apple"
+}
+
+// for (const key in myOnbject) {
+//     // console.log(key);
+// console.log(`${key} =is the ShortCut For The ${myOnbjec[key]}`);
+// }
+
+//****************For In Loop On Array **************
+
+// const arr = ["Aditya" , "Dhanorkar", "Raghu", "Patil"]
+
+// for (const key in arr) {
+//     console.log(arr[key]);
+// }
+
+//******For In Loop Not Work On Map ****************/
+
+// const map = new Map()
+// map.set('name' , "ADitya")
+// map.set('first',"Raghu")
+// map.set('last',"dhnaorkar")
+
+// for (const key in map) {
+//     console.log(key);
+// }
