4cfd8386d514361dc23229ffa107db33749b76fb
Aditya Parshram Dhanorkar <118669062+AdityaDhanorkar@users.noreply.github.com>
2024-07-15 13:51:05 +0000

    Promise

--- 09_Adavance/promise.js
+++ 09_Adavance/promise.js
@@ -46,3 +46,44 @@ PromiseFour.then((user)=>{
     console.log(error);
 }).finally(()=>{console.log("Resolve Or Reject");})
 
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({userName : "Raghu",email :"raghu@sample.com"})
+        }else{
+            reject("ERROR : Something Went Wrong")
+        }
+    },1000)
+})
+
+async function consumedPromiseFive(){
+    try{
+        const response = await promiseFive;
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+consumedPromiseFive()
+
+// async function AlldataFetch(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         // console.log(response);
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E : S W E")
+//     }
+// }
+
+// AlldataFetch()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response)=>{
+   return response.json();
+}).then((data)=>{console.log(data)})
+.catch((error)=>{console.log(error)})
